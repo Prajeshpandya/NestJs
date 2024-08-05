8a654deb869354b74999b8baeb2e8b524d027748
Parth Pandya <211290107020p@gmit.edu.in>
2024-08-05 17:25:08 +0530

    block specific domain's req

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, HostParam, Ip, Req } from '@nestjs/common';
 import { Request } from 'express';
 
-@Controller('/users') //the default router for the below funtions like expressJs.
+// @Controller({path: "/users" , host:"app.domain.com"}) //here we can specify the domain for taking request and accept!
+@Controller({ path: '/users', host: ':app.domain.com' }) //here we can also get dynamic Host and access that dynamic param.
+//the default router for the below funtions like expressJs.
 export class UsersController {
-
   @Get('/profile')
   getProfile(@Req() req: Request) {
     console.log(req); //with this @Req we can access the Request object and its values like Body,Params,Query etc..
     return 'Hello Parth!';
+  }
+  getParam(@HostParam() params: Record<string, any>) {
+    console.log(params); //with this @Req we can access the Request object and its values like Body,Params,Query etc..
+    return 'successfully get params domain.';
+  }
+  getIp(@Ip() ip: string) {
+    console.log(ip); 
+    return 'successfully get ip.';
   }
 }
