2a03c0cd93bc8aef4281617fe0921c5279205c00
Parth Pandya <211290107020p@gmit.edu.in>
2024-08-05 17:08:29 +0530

    complete the crud"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { AlbumController } from './albums.controller';
+import { Crud } from './crud.controller';
 
 @Module({
-  controllers: [UsersController,AlbumController],
+  controllers: [UsersController,AlbumController,Crud],
 })
 export class AppModule {
 
---
+++ src/crud.controller.ts
@@ -0,0 +1,61 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/createUserDto';
+
+let Users = [
+  {
+    name: 'Parth',
+    age: 20,
+    id: 123,
+  },
+  {
+    name: 'Ravindra',
+    age: 25,
+    id: 123456,
+  },
+];
+
+@Controller('/crud')
+export class Crud {
+  @Post()
+  createUser(@Body() createUser: CreateUserDto) {
+    Users.push(createUser);
+    return 'User Created!';
+  }
+
+  @Get()
+  getUsers() {
+    return Users;
+  }
+
+  @Get(':id')
+  getUser(@Param('id') id: number) {
+    return Users.find((user) => user.id === Number(id));
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: number, @Body() updateUser: CreateUserDto) {
+    const user = Users.find((user) => user.id === Number(id));
+    if (!user) return 'No User Found!';
+
+    user.age = updateUser.age;
+    user.name = updateUser.name;
+    user.id = updateUser.id;
+
+    return 'User Updated!';
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: number) {
+    Users= Users.filter((user) => user.id !== Number(id));
+
+    return 'User Delete!';  
+  }
+}
