0b612f6fd83e9ae6aadbca44b5c851a57f6a21fa
Parth Pandya <211290107020p@gmit.edu.in>
2024-08-05 14:16:15 +0530

    learn the response handling in nest

--- src/albums.controller.ts
+++ src/albums.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Redirect, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('/albums')
 export class AlbumController {
@@ -6,4 +7,18 @@ export class AlbumController {
   getAlbumName() {
     return 'This is my album';
   }
+
+  @Get('/redirect')
+  @Redirect("/albums/toRedirect")
+  redirectTheReq() {
+    return 'This is for redirect';
+  }
+
+  @Get('/toredirect')
+  toRedirect() {
+    return 'This is Redirected Req!';
+  }
+
+
+
 }
